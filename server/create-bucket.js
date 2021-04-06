2b03e5b7755fea5ee3b3100945f318e0fa6b25a6
Kay0s <hamilton.kristina@gmail.com>
2021-04-06 07:28:14 -0500

    updated user-images paramas on create-bucket.js

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -7,7 +7,7 @@ AWS.config.update({region: 'us-east-2'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 // Create the parameters for calling createBucket
 let bucketParams = {
-    Bucket : "user-images-" + uuidv4(),
+    Bucket : "user-images-1e09a9df-8970-4c93-a264-0223a8013463" + uuidv4(),
   };
   // call S3 to create the bucket
 s3.createBucket(bucketParams, (err, data) => {
